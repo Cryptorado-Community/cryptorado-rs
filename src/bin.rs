cb64ffea6651584f8c0000c894e835074e135b0c
Dan Shields <nukemandan@protonmail.com>
2023-10-27 20:01:43 -0600

    fix shuttle

--- src/bin.rs
+++ src/bin.rs
@@ -6,22 +6,24 @@ use anyhow::Result;
 
 /// Local main entrypoint.
 ///
-/// See also `main` in `lib.rs` - these will need to be manuall kept doing the same thing it seems 😭.
+/// See also `axum` - these will need to be manuall kept doing the same thing it seems 😭.
 #[cfg(feature = "local")]
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Serving at: http://127.0.0.1:5280");
+    println!("Serving at: http://127.0.0.1:8000");
     let router = cryptorado_rs::get_router();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 5280));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
         .await?;
     Ok(())
 }
 
-/// Dummy main, should never be encountered!
+/// The main function for running this the shuttle service.
+///
+/// See also `main` - these will need to be manuall kept doing the same thing it seems 😭.
 #[cfg(not(feature = "local"))]
-fn main() {
-    // dummy main function if shuttle is used
-    panic!("This main() should never run! Maybe you wanted\n\ncargo run --features local\n\nSee lib.rs for the shuttle main()")
+#[shuttle_runtime::main]
+async fn axum() -> shuttle_axum::ShuttleAxum {
+    Ok(cryptorado_rs::get_router().into())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,3 @@ use tower_http::services::ServeDir;
 pub fn get_router() -> Router {
     Router::new().nest_service("/", ServeDir::new("static"))
 }
-
-/// The main function for running this the shuttle service.
-///
-/// See also `main` in `bin.rs` - these will need to be manuall kept doing the same thing it seems 😭.
-#[cfg(feature = "shuttle")]
-#[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
-    Ok(get_router().into())
-}
